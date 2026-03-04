94b6332306fa53ad535dc78b9800fee250ab20fa
andiferataj <andi.fr33@gmail.com>
2026-03-04 16:46:09 +0100

    Lesson update
    Lessons 7 - 9

---
+++ Lesson7/connetionPDO.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+try{
+    $conn= new PDO("mysql:host=$host",$user,$pass);
+    echo "Connected";
+}catch(Exception $e){
+    echo "Not connected";
+}
+
+?>
